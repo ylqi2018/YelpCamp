a6f95318e398dbf74d5a797f1a7fcdc49d722425
ylqi007 <yunlongqi@my.unt.edu>
2017-11-16 13:22:00 -0600

    YelpCamp, V1_0.2

--- v1/app.js
+++ v1/app.js
@@ -5,6 +5,24 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+	{name: "Huangshan", image: "http://www.chinaculturetour.com/images/tours/huangshan/travel-seasons.jpg"},
+	{name: "Huashan", image: "https://photos.adventureinyou.com/wp-content/uploads/2016/09/22032514/mount-huashan-deadliest-climb_6-960x640.jpg"},
+	{name: "Taishan", image: "http://whc.unesco.org/uploads/thumbs/site_0437_0001-750-0-20080618083936.jpg"},
+	{name: "Huangshan", image: "http://www.chinaculturetour.com/images/tours/huangshan/travel-seasons.jpg"},
+	{name: "Huashan", image: "https://photos.adventureinyou.com/wp-content/uploads/2016/09/22032514/mount-huashan-deadliest-climb_6-960x640.jpg"},
+	{name: "Taishan", image: "http://whc.unesco.org/uploads/thumbs/site_0437_0001-750-0-20080618083936.jpg"},
+	{name: "Huangshan", image: "http://www.chinaculturetour.com/images/tours/huangshan/travel-seasons.jpg"},
+	{name: "Huashan", image: "https://photos.adventureinyou.com/wp-content/uploads/2016/09/22032514/mount-huashan-deadliest-climb_6-960x640.jpg"},
+	{name: "Taishan", image: "http://whc.unesco.org/uploads/thumbs/site_0437_0001-750-0-20080618083936.jpg"}
+]
+
+// ============================================================================
+// Routes
+// ============================================================================
+
+
+
 // Home page
 app.get('/', function(req, res){ 
 	//res.send("This will be the landing page.");
@@ -13,12 +31,24 @@ app.get('/', function(req, res){
 
 // View all campgrounds
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-	{name: "Huangshan", image: "http://www.chinaculturetour.com/images/tours/huangshan/travel-seasons.jpg"},
-	{name: "Huashan", image: "https://photos.adventureinyou.com/wp-content/uploads/2016/09/22032514/mount-huashan-deadliest-climb_6-960x640.jpg"},
-	{name: "Taishan", image: "http://whc.unesco.org/uploads/thumbs/site_0437_0001-750-0-20080618083936.jpg"}
-]
 	res.render("campgrounds", {campgrounds1: campgrounds});
+});
+
+// post routes
+app.post("/campgrounds", function(req, res) {
+	// get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	// res.send("You hit the POST ROUTE");
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	// redirect back to campgrounds page
+	res.redirect("/campgrounds");
+});
+
+// This route will send the data to the post route
+app.get("/campgrounds/new", function(req, res) {
+	res.render("new");
 });
 
 
--- v1/readme.md
+++ v1/readme.md
@@ -10,3 +10,21 @@
             image: String of url	
         }
     ]
+
+#Layout and Basic Styling
+* Create our header and footer partials
+* Add in Bootstrap
+
+#Creating new Campgrounds
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+#Style the campgrounds page
+* Add a better header/title
+* Make campgrounds display in a grid
+
+#Style the Navbar and Form
+* Add a new navbar to all templates
+* Style the new campground form
